546b4f992973ad89e71c17926a9fb0a122b90ac6
Jonathan <jon.d.elkouby@gmail.com>
2026-02-16 09:21:13 +0100

    Functioning draw function (copied over from Web Audio API documentation, will try to understand later) Issues: canvasContext definition doesn't seem to exist...

--- app.js
+++ app.js
@@ -19,19 +19,71 @@ startButton.addEventListener('click', () => {
     // Create Web Audio API context
     const audioContext = new AudioContext();
 
-    // Create the oscillator
+    
+    // (NODE) Create the oscillator
     const oscillator = audioContext.createOscillator();
-
+    
+    // (NODE) Create the analyser
+    const analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    
+    
+    
     // Set the frequency for the oscillator
     oscillator.frequency.value = freq.value;
-
+    
     // Set the amplitude for the oscillator
-    const gainNode = audioContext.createGain();
+    // (NODE)
+    const gainNode = audioContext.createGain(); 
     gainNode.gain.setValueAtTime(1, audioContext.currentTime);
-
-    oscillator.connect(gainNode);
+    
+    // connect the (NODE)s and start the oscillator, creating sound
+    oscillator.connect(analyser);
+    analyser.connect(gainNode);
     gainNode.connect(audioContext.destination);
     oscillator.start();
+    
+    // And create the canvas
+    const canvas = document.getElementById('oscilloscope');
+    const canvasContext = canvas.getContext('2d');
 
+    function draw() {
+        requestAnimationFrame(draw);
+
+        analyser.getByteTimeDomainData(dataArray);
+
+        canvasContext.fillStyle = 'rgb(200 200 200)';
+        canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+
+        canvasContext.lineWidth = 2;
+        canvasContext.strokeStyle = "rgb(0 0 0)";
+
+        canvasContext.beginPath();
+
+        const sliceWidth = (canvas.width * 1.0) / bufferLength;
+        let x = 0;
+
+        for (let i = 0; i < bufferLength; i++) {
+            const v = dataArray[i] / 128.0;
+            const y = (v * canvas.height) / 2;
+
+            if (i === 0) {
+                canvasContext.moveTo(x, y);
+            } else {
+                canvasContext.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+
+        canvasContext.lineTo(canvas.width, canvas.height / 2);
+        canvasContext.stroke();
+
+    }
+    
+    draw();
+    
 
 });
