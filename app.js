335738a58df349e1a8a7ecf2e2af80dc285dd6b9
Jonathan <jon.d.elkouby@gmail.com>
2026-03-11 10:56:20 +0100

    refactor: convert activeTabLink id to class

--- app.js
+++ app.js
@@ -263,18 +263,17 @@ newTabButton.addEventListener('click', () => {
     for (i = 0; i < tabArray.length; i++) {
         if(i == activeTab) {
             tabArray[i].tabHandle.innerHTML = `
-                <a id="activeTabLink">Oscillator ${i}</a>
+                <a class="activeTabLink">Oscillator ${i}</a>
             `
-            let activeTabLink = tabArray[i].tabHandle.querySelector('activeTabLink')
-            activeTabLink.addEventListener('click', () => {
-                
-            })
+            let activeTabLink = tabArray[i].tabHandle.querySelector('.activeTabLink')
         } else {
             tabArray[i].tabHandle.innerHTML = `
                 <a class="inactiveTabLink">Oscillator ${i}</a>
             `
             let inactiveTabLink = tabArray[i].tabHandle.querySelector('.inactiveTabLink');
-
+            inactiveTabLink.addEventListener('click', () => {
+                console.log('test')
+            });
         }
 
         tabBar.appendChild(tabArray[i].tabHandle);
